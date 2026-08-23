f5f209b5564e12e9927395e1bf60901a20f09120
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-08-23 13:38:12 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>hash(26, 0);
+        for(char ch: s){
+            hash[ch-'a']++;
+        }
+
+        for(char ch: t){
+            hash[ch-'a']--;
+        }    
+
+        for(int i=0; i<26; i++){
+            if(hash[i] != 0) return false;
+        }
+        return true;
+    }
+};
