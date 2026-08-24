698aede8b5c56339b19bc865a59ef9ef67faca76
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-08-24 13:22:54 +0530

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int maxProfit = 0;
+
+        for(int right=0; right<prices.size(); right++){
+            if(prices[left] >= prices[right]){
+                left = right;
+                continue;
+            } 
+
+            int profit = prices[right] - prices[left];
+            maxProfit = max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+};
