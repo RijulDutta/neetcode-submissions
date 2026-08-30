f885c44c75701cba403ee04eaab389adefa43388
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-08-30 20:44:49 +0530

    Add: kth-smallest-integer-in-bst - submission-3

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-3.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    priority_queue<int>pq;
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return pq.top();
+    }
+
+    void inorder(TreeNode* root, int k){
+        if(root == nullptr) return;
+
+        inorder(root->left, k);
+        pq.push(root->val);
+        if(pq.size() > k){
+            pq.pop();
+        }
+        inorder(root->right, k);
+    }
+};
