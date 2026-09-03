618881d3b6a522f3c7f56a77a89c7e5749e1014d
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-09-03 09:09:05 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string>ans;
+    int n;
+    vector<string> generateParenthesis(int n) {
+        this->n = n;
+        string inter;
+        helper(inter, 0, 0);
+        return ans;
+    }
+
+    void helper(string inter, int open, int close){
+        if(open == n && close == n){
+            ans.push_back(inter);
+            return;
+        }
+
+        if(open < n){
+            inter.push_back('(');
+            helper(inter, open+1, close);
+            inter.pop_back();
+        }
+
+        if(close < open){
+            inter.push_back(')');
+            helper(inter, open, close+1);
+            inter.pop_back();
+        }
+    }
+};
