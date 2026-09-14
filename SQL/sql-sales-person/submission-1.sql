6e3bb90e3f44270175ac0821b04f02e0ca9d64cc
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-09-14 11:16:59 +0530

    Add: sql-sales-person - submission-1

---
+++ SQL/sql-sales-person/submission-1.sql
@@ -0,0 +1,16 @@
+--Write your query below
+SELECT name
+FROM sales_person 
+WHERE sales_id NOT IN (
+    SELECT DISTINCT o.sales_id
+    FROM orders o
+    LEFT JOIN company c
+    ON o.com_id = c.com_id
+    WHERE c.name = 'CRIMSON'
+)
+
+-- SELECT DISTINCT o.sales_id
+-- FROM orders o
+-- LEFT JOIN company c
+-- ON o.com_id = c.com_id
+-- WHERE c.name = 'CRIMSON'
