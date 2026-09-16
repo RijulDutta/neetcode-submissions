03d0c84ea3076336a75501b787871ba28dc04b0c
RijulDutta <148096855+RijulDutta@users.noreply.github.com>
2026-09-16 08:24:19 +0530

    Add: pacific-atlantic-water-flow - submission-6

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-6.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int row;
+    int col;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>>ans;
+        row = heights.size();
+        col = heights[0].size();
+
+        vector<vector<bool>>pacific(row, vector<bool>(col, false));
+        vector<vector<bool>>atlantic(row, vector<bool>(col, false));
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(i==0 || j==0) pacific[i][j] = true;
+                if(i == row-1 || j == col-1) atlantic[i][j] = true;
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(pacific[i][j] == true){
+                    helper(heights, i, j, pacific);
+                }
+                if(atlantic[i][j] == true){
+                    helper(heights, i, j, atlantic);
+                }
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(pacific[i][j] == true && atlantic[i][j] == true){
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    void helper(vector<vector<int>>&heights, int i, int j, vector<vector<bool>>&matrix){
+        matrix[i][j] = true;
+
+        if(i+1 < row && heights[i+1][j] >= heights[i][j] && !matrix[i+1][j]) {   
+            helper(heights, i+1, j, matrix);
+        }
+        if(i-1 >= 0 && heights[i-1][j] >= heights[i][j] && !matrix[i-1][j]) {
+            helper(heights, i-1, j, matrix);
+        }
+        if(j+1 < col && heights[i][j+1] >= heights[i][j] && !matrix[i][j+1]){
+            helper(heights, i, j+1, matrix);
+        }
+        if(j-1 >= 0 && heights[i][j-1] >= heights[i][j] && !matrix[i][j-1]){
+            helper(heights, i, j-1, matrix);
+        }
+    }
+};
